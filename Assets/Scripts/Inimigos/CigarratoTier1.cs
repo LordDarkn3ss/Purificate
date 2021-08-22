c37b42295b2cdc300793a85b46ddae64f541ef4b
J. Victor <49132641+LordDarkn3ss@users.noreply.github.com>
2021-08-22 08:47:31 -0300

    Purificate 0.8
    trocando sprites das pedras por QUAD

--- Assets/Scripts/Inimigos/CigarratoTier1.cs
+++ Assets/Scripts/Inimigos/CigarratoTier1.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class CigarratoTier1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform escaner; // o escaner / olho / visão do inimigo
+    public float distanciaCheckAbism, speed; // um ranger da visão de abismo e uma variavel de velocidade
+    public bool attackRanger, direita; // um BOOL pra saber se o player tá no ranger de ataque, e um pra saber a direção a seguir
+
+    public  Animator tier1; // animator controller do Cigarratu Tier 1
+
     void Start()
     {
-        
+        tier1 = GetComponent<Animator>(); // Recebendo o componente de animator
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+            
+       
+        if(!attackRanger){ //Se não estiver dentro do Ranger de ataque, continue andando
+        
+         Move(); // movendo
+        }
+        else
+        {
+           tier1.Play("Attack"); //rodar animação de ataque
+            
+        }
+
+    }
+    void Move()
+    {
+        if(Physics.Raycast(escaner.position,-escaner.up,distanciaCheckAbism))   //detectar abismo
+         {
+             Debug.DrawRay(escaner.position,new Vector3(0,-distanciaCheckAbism,0),Color.red);
+
+             
+            transform.Translate(Vector2.right * speed * Time.deltaTime); //Movimenta o inimigo           
+         }
+         else if(direita)
+         {
+             transform.eulerAngles = new Vector3(0, 180, 0); //girar inimigo
+                direita=false;
+         }
+         else
+         {
+             transform.eulerAngles = new Vector3(0, 0, 0); //girar inimigo
+                direita=true;
+         }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") // descobrir que o player está dentro do ranger de ataque
+        {
+            attackRanger = true; // disse que tá no ranger de ataque
+        }
     }
 }
